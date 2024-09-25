5f01ac8c0931932b19b6dc0188138c3c3b2f7a14
zach <zach@dylibso.com>
2024-09-25 12:46:06 -0700

    fix: build.rs when EXTISM_ENGINE_PATH is set

--- bin/build.rs
+++ bin/build.rs
@@ -1,7 +1,12 @@
 fn main() {
     println!("cargo::rerun-if-changed=src/invoke.py");
-    println!("cargo::rerun-if-changed=../lib/target/wasm32-wasi/release/core.wasm");
 
     let out = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("core.wasm");
-    std::fs::copy("../lib/target/wasm32-wasi/release/core.wasm", out).unwrap();
+    if let Ok(path) = std::env::var("EXTISM_ENGINE_PATH") {
+        println!("cargo::rerun-if-changed={path}");
+        std::fs::copy(path, out).unwrap();
+    } else {
+        println!("cargo::rerun-if-changed=../lib/target/wasm32-wasi/release/core.wasm");
+        std::fs::copy("../lib/target/wasm32-wasi/release/core.wasm", out).unwrap();
+    }
 }
